a45f47c6b2700bfae6efcbd5c80c4f44a5516fb4
Jonas Rakebrandt <xarblu@protonmail.com>
2023-01-20 19:47:51 +0100

    sci-electronics/digital: update wrapper

--- sci-electronics/digital/files/wrapper.sh
+++ sci-electronics/digital/files/wrapper.sh
@@ -3,4 +3,6 @@
 # Wrap the jar file
 DIR="%%INSTALLDIR%%"
 JAR="%%JAR%%"
-java -jar "${DIR}/${JAR}" "${@}"
+java -Dawt.useSystemAAFontSettings=on \
+     -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel \
+     -jar "${DIR}/${JAR}" "${@}"
